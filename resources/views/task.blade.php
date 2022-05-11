f02377c465a91d4e87d9a7972a0d4055659a6be6
elham21m <71438103+elham21m@users.noreply.github.com>
2022-05-11 09:10:20 +0200

    Update task.blade.php

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -6,7 +6,7 @@
     <div class="card-content">
       <div class="content">
         <h1>{{ $task->title }}</h1>
-        <h4>{{ $task->description }}</h4>
+        <p>{{ $task->description }}</p>
         <h6><em>Created in :
           <?php echo Carbon\Carbon::parse($task->created_at)->format('d M Y') ?>
           </em></h6>
